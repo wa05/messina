cbf3e8ca4f790d0b47b3672a4b18dd4092088b25
wa05 <waltercejas19@hotmail.com>
2016-03-21 08:20:22 -0300

    Working on table

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -2,44 +2,59 @@
 
 angular.module('myApp.view2', [])
 
-.controller('View2Ctrl', ['$scope',function($scope) {
+.controller('View2Ctrl', 
+	['$scope','$filter',function($scope,$filter) {
 	
 	$scope.hoy = new Date();
-
-
 
 	var control = [
 		{
 			'repartidor':'Pablo',
-			'date' : '03/03/2016',
+			'fecha' : '03/03/2016',
 			'xdoce': 15,
 			'xveinte': 19,
-			'soda':23
+			'soda':23,
+			'totaldia':1200
 		},
 		{
 			'repartidor':'Marcelo P',
-			'date' : '04/03/2016',
+			'fecha' : '04/03/2016',
 			'xdoce': 5,
 			'xveinte': 9,
-			'soda':23
+			'soda':23,
+			'totaldia':1400
 		},
 		{
 			'repartidor':'Gustavo',
-			'date': '07/03/2016',
+			'fecha': '07/03/2016',
 			'xdoce': 12,
 			'xveinte': 17,
-			'soda':23
+			'soda':23,
+			'totaldia':2400
 		}
 	];
 	$scope.pablo = {};
 	$scope.pablo.control = control;
 
-	console.log($scope.pablo);
-
 	$scope.new={};
+	$scope.new.fecha = $scope.hoy;
 
 	$scope.save = function(){
 		$scope.pablo.control.push($scope.new);
 		$scope.new = {};
+	$scope.new.fecha = $scope.hoy;
 	};
+
+	var orderBy = $filter('orderBy');
+
+	$scope.order = function(predicate){
+		$scope.pablo.control= orderBy($scope.pablo.control,predicate);
+		/*
+		$scope.predicate = predicate;
+		$scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+		$scope.control = orderBy($scope.control, predicate, $scope.reverse);
+	*/
+	};
+	$scope.order('fecha',true);
+
 }]);
